13518b4be989b2a07b2a392218bd06990c5474fb
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-10-31 19:14:01 -0400

    shrink bundle

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { MarkdownTransformer } from '@atlaskit/editor-markdown-transformer'
-import * as core from '@actions/core'
-import * as github from '@actions/github'
+import {
+	setOutput,
+	info as logInfo,
+	error as logError,
+	setFailed,
+} from '@actions/core'
+import { context } from '@actions/github'
 import type { IssuesOpenedEvent } from '@octokit/webhooks-definitions/schema'
 const transformer = new MarkdownTransformer()
 
@@ -9,18 +14,18 @@ const md2adf = (markdown: string) => {
 }
 
 try {
-	const payload = github.context.payload as IssuesOpenedEvent
+	const payload = context.payload as IssuesOpenedEvent
 	const jiraBody = `${payload.issue.html_url}\n\n${payload.issue.body}`
 	const adf = md2adf(jiraBody)
 
-	core.setOutput('adf', adf)
-	core.info('Issue body converted')
+	setOutput('adf', adf)
+	logInfo('Issue body converted')
 } catch (error) {
 	if (error instanceof Error) {
-		core.error(error.message)
-		core.setFailed(error.message)
+		logError(error.message)
+		setFailed(error.message)
 	} else {
-		core.error(error as string)
-		core.setFailed(error as string)
+		logError(error as string)
+		setFailed(error as string)
 	}
 }
